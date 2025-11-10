fafc1871fc4b48473710d1df334f7d4e9eab581f
Ivan Kekar <vdeynesheko20@gmail.com>
2025-11-10 22:38:41 +0400

    finish arrows funtionality

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const changeWelcomeImage = (value) => {
 
     const welcomeBackgroundBlock = document.querySelector('.welcome_wrapper');
+    const pages = document.querySelectorAll('.page')
+
     let numberOfPage = document.querySelector('#changable_number');
+    let activePage = document.querySelector('#active');
 
     switch (value) {
         case 'rightArrow':
@@ -17,11 +20,30 @@ const changeWelcomeImage = (value) => {
             }
 
             welcomeBackgroundBlock.style.backgroundImage = `url(/assets/img/welcome-slider/${Number(numberOfPage.textContent)}.jpg)`;
-
+        
             break;
     
+        case 'leftArrow':
+
+            if (numberOfPage.textContent == '1') {
+
+                numberOfPage.textContent = 5;
+
+            } else {
+
+                numberOfPage.textContent = Number(numberOfPage.textContent)-1;
+
+            }
+
+            welcomeBackgroundBlock.style.backgroundImage = `url(/assets/img/welcome-slider/${Number(numberOfPage.textContent)}.jpg)`;
+            
+            break;
+
         default:
             break;
     }
+
+    activePage.removeAttribute('id');
+    pages[numberOfPage.textContent-1].setAttribute('id', 'active')
 
 }
