6f7ccf5ded7697f480ee5c7ac41a749ebca5b7b0
Ivan Kekar <vdeynesheko20@gmail.com>
2025-11-19 16:05:21 +0400

    add files and translate code from main js to relative files

--- scripts/welcome_slider.js
+++ scripts/welcome_slider.js
@@ -1,25 +1,10 @@
 const welcomeSliderWrapper = document.querySelector('.slider_wrapper_welcome');
 const sliderWelcome = welcomeSliderWrapper.querySelector('.slider_welcome');
-
-const pages = document.querySelectorAll('.page');
-const exploreBlock = document.querySelector(".picture");
-const imageBefore = document.querySelector('.image_before');
-const slider = document.querySelector('.slider_block');
-
 const gapBetweenSlides = 15;
-
-const sliderExploreObject = {
-
-    positionPictureExploreBlock: exploreBlock.getBoundingClientRect().left,
-    maxDistanceForSlider: 700,
-    minDistanceForSlider: -20,
-    maxDistanceForImage: 700,
-    minDistanceForImage: 0,
-
-}
 
 const sliderWelcomeObject = {
 
+    
     firstImagePosition:  0,
     secondImagePosition: 1000 + gapBetweenSlides,
     thirdImagePosition:  2000 + gapBetweenSlides,
@@ -32,6 +17,23 @@ const sliderWelcomeObject = {
     centerOfPictureWelcomeBlock: welcomeSliderWrapper.getBoundingClientRect().width/2,
 
     speedDragging: 0.35,
+    
+}
+
+const sliderWelcomeActive = (target) => {
+    let startX = target.clientX;
+    let newPos = startX - sliderWelcomeObject.positionPictureWelcomeBlock - sliderWelcomeObject.centerOfPictureWelcomeBlock;
+
+    let currentX = newPos * sliderWelcomeObject.speedDragging;
+    
+    sliderWelcome.style.backgroundPositionX = `
+        ${sliderWelcomeObject.firstImagePosition + currentX}px,
+        ${sliderWelcomeObject.secondImagePosition + currentX}px,
+        ${sliderWelcomeObject.thirdImagePosition + currentX}px,
+        ${sliderWelcomeObject.fourthImagePosition + currentX}px,
+        ${sliderWelcomeObject.fiveImagePosition + currentX}px`;
+
+    sliderWelcome.style.transition = `0s`;    
 
 }
 
@@ -84,76 +86,7 @@ const onMouseUpChangeSliderImage = (target) => {
 
 }
 
-const sliderWelcomeActive = (target) => {
-    let startX = target.clientX;
-    let newPos = startX - sliderWelcomeObject.positionPictureWelcomeBlock - sliderWelcomeObject.centerOfPictureWelcomeBlock;
 
-    let currentX = newPos * sliderWelcomeObject.speedDragging;
-    
-    sliderWelcome.style.backgroundPositionX = `
-        ${sliderWelcomeObject.firstImagePosition + currentX}px,
-        ${sliderWelcomeObject.secondImagePosition + currentX}px,
-        ${sliderWelcomeObject.thirdImagePosition + currentX}px,
-        ${sliderWelcomeObject.fourthImagePosition + currentX}px,
-        ${sliderWelcomeObject.fiveImagePosition + currentX}px`;
-
-    sliderWelcome.style.transition = `0s`;    
-
-}
-
-const sliderExploreActive = (target) => {
-
-    let newPos = target.clientX - sliderExploreObject.positionPictureExploreBlock;
-
-    slider.style = `left: ${newPos}px;`;
-    imageBefore.style = `width: ${newPos+20}px;`;
-    
-    if (newPos >= sliderExploreObject.maxDistanceForSlider) {
-
-        slider.style = `left: ${sliderExploreObject.maxDistanceForSlider}px;`;
-        imageBefore.style = `width: ${sliderExploreObject.maxDistanceForImage}px;`;
-
-    }
-
-    if (newPos <= sliderExploreObject.minDistanceForSlider) {
-
-        slider.style = `left: ${sliderExploreObject.minDistanceForSlider}px;`;
-        imageBefore.style = `width: ${sliderExploreObject.minDistanceForImage}px;`;
-
-    }
-
-}
-
-
-const handleRemoveSliderExploreListener = () => {
-
-    exploreBlock.removeEventListener('mouseup', handleRemoveSliderExploreListener);
-    exploreBlock.removeEventListener('mousemove', sliderExploreActive);
-
-}
-
-const handleSliderExplore = () => {
-
-    exploreBlock.addEventListener('mouseup', handleRemoveSliderExploreListener);
-    exploreBlock.addEventListener('mousemove', sliderExploreActive);
-
-}
-
-const handleRemoveSliderWelcomeListener = () => {
-
-    welcomeSliderWrapper.removeEventListener('mouseup', handleRemoveSliderWelcomeListener);
-    welcomeSliderWrapper.removeEventListener('mousemove', sliderWelcomeActive);
-    sliderWelcome.removeEventListener('mouseup', onMouseUpChangeSliderImage);
-
-}
-
-const handleSliderWelcome = () => {
-    
-    welcomeSliderWrapper.addEventListener('mouseup', handleRemoveSliderWelcomeListener);
-    welcomeSliderWrapper.addEventListener('mousemove', sliderWelcomeActive);
-    sliderWelcome.addEventListener('mouseup', onMouseUpChangeSliderImage);
-
-}
 
 const changeWelcomeImagePagination = (value) => {
 
@@ -290,14 +223,32 @@ const changeWelcomeImagePagination = (value) => {
             break;
     }
 
-    sliderWelcome.style = `background-position-x: 
-        ${sliderWelcomeObject.firstImagePosition}px,
+    sliderWelcome.style.backgroundPositionX = 
+        `${sliderWelcomeObject.firstImagePosition}px,
         ${sliderWelcomeObject.secondImagePosition}px,
         ${sliderWelcomeObject.thirdImagePosition}px,
         ${sliderWelcomeObject.fourthImagePosition}px,
-        ${sliderWelcomeObject.fiveImagePosition}px;`;
+        ${sliderWelcomeObject.fiveImagePosition}px`;
+    sliderWelcome.style.transition = '1s';
 
     activePage.removeAttribute('id');
     pages[numberOfPage.textContent-1].setAttribute('id', 'active')
+
+}
+
+const handleRemoveSliderWelcomeListener = () => {
+
+    welcomeSliderWrapper.removeEventListener('mouseup', handleRemoveSliderWelcomeListener);
+    welcomeSliderWrapper.removeEventListener('mousemove', sliderWelcomeActive);
+    sliderWelcome.removeEventListener('mouseup', onMouseUpChangeSliderImage);
+
+}
+
+
+const handleSliderWelcome = () => {
+    
+    welcomeSliderWrapper.addEventListener('mouseup', handleRemoveSliderWelcomeListener);
+    welcomeSliderWrapper.addEventListener('mousemove', sliderWelcomeActive);
+    sliderWelcome.addEventListener('mouseup', onMouseUpChangeSliderImage);
 
 }
