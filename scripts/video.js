5b372468d051db3dfcc0a33b66c398824e2c1461
Ivan Kekar <vdeynesheko20@gmail.com>
2025-11-19 16:19:13 +0400

    selected based controls in main video and add script in HTML

---
+++ scripts/video.js
@@ -0,0 +1,14 @@
+const mainVideo = document.querySelector('.main_video').querySelector('video');
+
+const controlPanel = document.querySelector('.control_panel');
+const bigPlayButton = controlPanel.querySelector('.button_play');
+
+const playButtonInPanel = controlPanel.querySelector('.button_play_by_panel_image');
+const timeline = controlPanel.querySelector('.timeline');
+const dotInTimeline = timeline.querySelector('.dot');
+
+const volumeImage = controlPanel.querySelector('.volume_image');
+const volumeLine = controlPanel.querySelector('.volume_line');
+const dotInVolume = volumeLine.querySelector('.dot');
+
+const fullscreen = controlPanel.querySelector('.fullscreen_image');
