f82964de390b5633a1226331398746d56d3bcc06
Ivan Kekar <vdeynesheko20@gmail.com>
2025-12-01 21:06:21 +0400

    add animations for close and open header

--- scripts/burgerMenu.js
+++ scripts/burgerMenu.js
@@ -2,18 +2,75 @@ const burgerMenu = document.querySelector('.burder_svg');
 const burgerCloseButton = document.querySelector('.cross_svg');
 const navigation = document.querySelector('.navigation ');
 
+const animationArrayForOpen = [
+
+    {transform: "translateX(-380px)", opacity: '0'},
+    {transform: "translateX(0px)", opacity: '1'}
+    
+];
+
+const animationArrayForClose = [
+
+    {transform: "translateX(0px)", opacity: '1'},
+    {transform: "translateX(-380px)", opacity: '0'}
+    
+];
+
+const animationArrayForBurger = [
+
+    {opacity: '0'},
+    {opacity: '1'}
+    
+];
+
+const animationArrayForCross = [
+
+    {opacity: '0'},
+    {opacity: '1'}
+    
+];
+
+const durationArray = {
+
+    duration: 650,
+
+}
+
 const WIDTH = window.innerWidth
 
 const openBurger = () => {
 
-    navigation.style.display = 'block';
-
     
+
+    burgerMenu.style.display = 'none';
+    burgerCloseButton.style.display = 'block';
+
+    burgerMenu.animate(animationArrayForBurger, durationArray)
+    burgerCloseButton.animate(animationArrayForCross, durationArray)
+
+    navigation.animate(animationArrayForOpen, durationArray)
+
+    navigation.style.transform = 'translateX(0px)';
+
+}
+
+const closeBurger = () => {
+    
+    burgerMenu.animate(animationArrayForBurger, durationArray)
+    burgerCloseButton.animate(animationArrayForCross, durationArray)
+    navigation.animate(animationArrayForClose, durationArray)
+
+    navigation.style.transform = 'translateX(-380px)';
+
+    burgerMenu.style.display = 'block';
+    burgerCloseButton.style.display = 'none';
 
 }
 
 if (WIDTH == 1024) {
 
     burgerMenu.addEventListener('click', openBurger)
+
+    burgerCloseButton.addEventListener('click', closeBurger)
 
 }
