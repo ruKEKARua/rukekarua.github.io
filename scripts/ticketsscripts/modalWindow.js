e6916babdc972051310b67259929997cb633d374
Ivan Kekar <vdeynesheko20@gmail.com>
2025-12-02 21:46:29 +0400

    counters cange in modal window

--- scripts/ticketsscripts/modalWindow.js
+++ scripts/ticketsscripts/modalWindow.js
@@ -1,6 +1,9 @@
-const applicantForm = document.querySelector('#formTickets');
+const applicantForm = document.querySelector('.modal_wrapper');
 const sectionWrapper = document.querySelector('.tickets_wrapper');
 const modalWindow = document.querySelector('.booking_modal');
+
+const counterBasic = applicantForm.querySelector('#inputBookingCountBasic');
+const counterSenior = applicantForm.querySelector('#inputBookingCountSenior');
 
 const closeModal = () => {
 
@@ -9,4 +12,44 @@ const closeModal = () => {
 
 }
 
-export { closeModal }
+const handleCountBasicPlusModal = () => {
+
+    counterBasic.value = Number(counterBasic.value) + 1;
+
+};
+
+const handleCountBasicMinusModal = () => {
+
+    if (Number(counterBasic.value) - 1 >= 0) {
+
+        counterBasic.value = Number(counterBasic.value) - 1;
+
+    }
+
+};
+
+const handleCountSeniorPlusModal = () => {
+
+    counterSenior.value = Number(counterSenior.value) + 1;
+
+};
+
+const handleCountSeniorMinusModal = () => {
+
+    if (Number(counterSenior.value) - 1 >= 0) {
+
+        counterSenior.value = Number(counterSenior.value) - 1;
+
+    }
+
+};
+
+export { 
+
+    closeModal,
+    handleCountBasicPlusModal,
+    handleCountBasicMinusModal,
+    handleCountSeniorPlusModal,
+    handleCountSeniorMinusModal
+
+}
--- scripts/ticketsscripts/tickets.js
+++ scripts/ticketsscripts/tickets.js
@@ -4,16 +4,32 @@ import {
     handleCountSeniorPlus,
     handleCountSeniorMinus 
 } from "./counterButtons.js";
-import { closeModal } from "./modalWindow.js";
+import { 
+    closeModal, 
+    handleCountBasicPlusModal,
+    handleCountBasicMinusModal,
+    handleCountSeniorPlusModal,
+    handleCountSeniorMinusModal
+
+} from "./modalWindow.js";
 
 const applicantForm = document.querySelector('#formTickets');
+const applicantFormModal = document.querySelector('.modal_wrapper');
+
 const sectionWrapper = document.querySelector('.tickets_wrapper');
 const modalWindow = document.querySelector('.booking_modal');
 
 const plusButtonBasic = applicantForm.querySelector('#plusButtonBasic')
 const minusButtonBasic = applicantForm.querySelector('#minusButtonBasic')
+
 const plusButtonSenior = applicantForm.querySelector('#plusButtonSenior')
 const minusButtonSenior = applicantForm.querySelector('#minusButtonSenior')
+
+const plusButtonBasicModal = applicantFormModal.querySelector('#ticketButtonPlusBasic')
+const minusButtonBasicModal = applicantFormModal.querySelector('#ticketButtonMinusBasic')
+
+const plusButtonSeniorModal = applicantFormModal.querySelector('#ticketButtonPlusSenior')
+const minusButtonSeniorModal = applicantFormModal.querySelector('#ticketButtonMinusSenior')
 
 const closeModalButton = document.querySelector('.close_button');
 
@@ -44,4 +60,9 @@ minusButtonBasic.addEventListener('click', handleCountBasicMinus)
 plusButtonSenior.addEventListener('click', handleCountSeniorPlus)
 minusButtonSenior.addEventListener('click', handleCountSeniorMinus)
 
-closeModalButton.addEventListener('click', closeModal)
+plusButtonBasicModal.addEventListener('click', handleCountBasicPlusModal);
+minusButtonBasicModal.addEventListener('click', handleCountBasicMinusModal);
+plusButtonSeniorModal.addEventListener('click', handleCountSeniorPlusModal);
+minusButtonSeniorModal.addEventListener('click', handleCountSeniorMinusModal);
+
+closeModalButton.addEventListener('click', closeModal);
